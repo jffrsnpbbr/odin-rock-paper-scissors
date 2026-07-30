0ad345300949dd7cfe7d0cccb688733642d19907
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2026-07-31 01:32:48 +0800

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+// will write a function that randomly returns “rock”, “paper” or “scissors”. -- done
+// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”. -- done
+
+function  getComputerChoice() {
+  const randomIndex = Math.floor(Math.random() * 3); // returns random number betwee 0 and 2
+
+  if (randomIndex === 1) {
+    return "rock";
+  } else if (randomIndex === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+const choice = getComputerChoice();
+console.log(choice);
