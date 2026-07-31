a8dc4a68dadb91c7de573c4a10fdbc2cc424b6f7
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2026-07-31 12:29:07 +0800

    Add players score variables

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // will write a function that randomly returns “rock”, “paper” or “scissors”. -- done
 // Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”. -- done
 
+let humanScore = 0;
+let computerScore = 0;
+
 function  getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3); // returns random number betwee 0 and 2
 
@@ -13,8 +16,6 @@ function  getComputerChoice() {
   }
 }
 
-
-
 function getHumanChoice() {
   const humanChoice = prompt("Please enter your choice: rock, paper, or scissors").toLowerCase();
   if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
@@ -25,8 +26,9 @@ function getHumanChoice() {
   return;
 }
 
-// const computerChoice = getComputerChoice();
 const humanChoice = getHumanChoice();
 const computerChoice = getComputerChoice();
 console.log(`Human choice: ${humanChoice}`);
 console.log(`Computer choice: ${computerChoice}`);
+
+
